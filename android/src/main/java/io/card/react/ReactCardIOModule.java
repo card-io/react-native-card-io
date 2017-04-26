e8c6405e759f2ca8d8a998fa0d111566c28474a2
Tim Matthews <tim@3sidedcube.com>
2017-04-26 13:49:01 +0100

    Fixed RESULT_CONFIRMATION_SUPPRESSED being reported as an error to JS code

--- android/src/main/java/io/card/react/ReactCardIOModule.java
+++ android/src/main/java/io/card/react/ReactCardIOModule.java
@@ -79,7 +79,7 @@ public class ReactCardIOModule extends ReactContextBaseJavaModule implements Act
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
       Log.i(TAG,"Activity Result" + requestCode + " result Code " + resultCode);
       if (REQUEST_CARD_SCAN == requestCode) {
-            if (resultCode == CardIOActivity.RESULT_CARD_INFO) {
+            if (resultCode == CardIOActivity.RESULT_CARD_INFO || resultCode == CardIOActivity.RESULT_CONFIRMATION_SUPPRESSED) {
                 CreditCard scanResult = null;
                 if (intent.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT)) {
                     scanResult = intent
